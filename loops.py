ed6cd8b371cfee3b9fcfc8eadbbd52c3429d3405
shahid_nawaz <shahidnawaz.edu.pk@gmail.com>
2023-03-23 02:51:43 +0500

    for loop and while loop

---
+++ loops.py
@@ -0,0 +1,13 @@
+names = ['shahid','asad','amir','zahid']
+for name in names:
+    print(f'the name is {name}')
+
+
+for x in range(1,10):
+    print(x)
+
+
+x=1
+while x <= 10:
+    print(x)
+    x+=1
