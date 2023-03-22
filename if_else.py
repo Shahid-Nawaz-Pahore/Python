302ebb1bf339d9335be70bf8e3ff85b616d56d52
shahid_nawaz <shahidnawaz.edu.pk@gmail.com>
2023-03-23 01:33:38 +0500

    if else statement examples

---
+++ if_else.py
@@ -0,0 +1,15 @@
+name = "shahid"
+age = 23
+profession = "student"
+grade="BSCS"
+# if statement using string in condition
+if name == 'shahid':
+    print('your are shahid')
+else:
+    print('your are not shahid')
+
+# if statement using integer in condition
+if age == 23:
+    print(f'your age is {age}')
+else:
+    print(f'your age is not {age}')
