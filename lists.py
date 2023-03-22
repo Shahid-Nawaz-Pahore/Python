4f985f6e635c6f6afa1c81075a34f1ea3b1a2ee3
shahid_nawaz <shahidnawaz.edu.pk@gmail.com>
2023-03-23 02:34:54 +0500

    define list , display list ,display spacific value in list,add value in list delete value from list

---
+++ lists.py
@@ -0,0 +1,13 @@
+
+name = ['shahid','asad','amir','zahid']
+print(name)
+#print spacific value in list
+print(name[1])
+#add other value in list
+name.append('kamran')
+print(name)
+
+#delete value from list
+name.remove(name[1])
+print(name)
+
