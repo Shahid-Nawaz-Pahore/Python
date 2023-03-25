a4c0d184baab9f77476f2c318b5684d86d6f7b6a
shahid_nawaz <shahidnawaz.edu.pk@gmail.com>
2023-03-26 01:35:02 +0500

    name replace with first name

--- distionaries.py
+++ distionaries.py
@@ -1,11 +1,11 @@
 record = {
-    'name':'shahid',
+    'first name':'shahid',
     'roll_no':60,
     'age': 23,
 
 }
 print(record)
 
-print(record['name'])
+print(record['first name'])
 print(record['roll_no'])
 print(record['age'])
